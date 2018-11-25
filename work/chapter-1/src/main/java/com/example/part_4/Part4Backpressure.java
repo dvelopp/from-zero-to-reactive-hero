a28081cf4d7925d4fd93e824e74554adf9d9e957
dmitrypashkov <dmitry.pashkov@nextiva.com>
2018-11-25 17:47:51 +0200

    First Four

--- work/chapter-1/src/main/java/com/example/part_4/Part4Backpressure.java
+++ work/chapter-1/src/main/java/com/example/part_4/Part4Backpressure.java
@@ -1,12 +1,11 @@
 package com.example.part_4;
 
-import com.example.annotations.Complexity;
-import com.example.common.StringEventPublisher;
-import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.util.List;
+
+import com.example.annotations.Complexity;
 
 import static com.example.annotations.Complexity.Level.EASY;
 import static com.example.annotations.Complexity.Level.MEDIUM;
@@ -15,17 +14,13 @@ public class Part4Backpressure {
 
     @Complexity(EASY)
     public static Flux<String> dropElementsOnBackpressure(Flux<String> upstream) {
-        // TODO: apply backpressure to drop elements on downstream overwhelming
-        // HINT: Flux#onBackpressureDrop
+        return upstream.onBackpressureDrop();
 
-        throw new RuntimeException("Not implemented");
     }
 
     @Complexity(MEDIUM)
     public static Flux<List<Long>> backpressureByBatching(Flux<Long> upstream) {
-        // TODO: decrease emission rate by buffering elements during the second
-        // HINT: Flux#window(Duration) + .flatMap( .collectList ) or MORE simply Flux#buffer(Duration)
-
-        throw new RuntimeException("Not implemented");
+        return upstream.window(Duration.ofMillis(1000)).flatMap(Flux::collectList);
     }
+
 }
--- work/chapter-1/src/main/java/com/example/part_4/Part4ExtraBackpressure_Optional.java
+++ work/chapter-1/src/main/java/com/example/part_4/Part4ExtraBackpressure_Optional.java
@@ -1,35 +1,36 @@
 package com.example.part_4;
 
+import reactor.core.publisher.Flux;
+
 import java.util.concurrent.CountDownLatch;
+
+import org.reactivestreams.Publisher;
 
 import com.example.annotations.Complexity;
 import com.example.annotations.Optional;
 import com.example.common.StringEventPublisher;
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscription;
-import reactor.core.publisher.BaseSubscriber;
-import reactor.core.publisher.Flux;
 
 import static com.example.annotations.Complexity.Level.MEDIUM;
 
 public class Part4ExtraBackpressure_Optional {
 
-	@Complexity(MEDIUM)
-	public static Publisher<String> handleBackpressureWithBuffering(StringEventPublisher stringEventPublisher) {
-		// TODO: adapt non-Reactor api and apply backpressure strategy
-		// HINT: Flux.create or Flux.push
-		throw new RuntimeException("Not implemented");
-	}
+    @Complexity(MEDIUM)
+    public static Publisher<String> handleBackpressureWithBuffering(StringEventPublisher stringEventPublisher) {
+        return Flux.<String>create(sink -> stringEventPublisher.registerEventListener(sink::next))
+                .buffer(1)
+                .flatMap(Flux::fromIterable);
+    }
 
-	@Optional
-	@Complexity(MEDIUM)
-	public static void dynamicDemand(Flux<String> source, CountDownLatch countDownOnComplete) {
-		// TODO: provide own implementation of Subscriber. Manage request count manually.
-		// TODO: two times increase request size each time when previous demand has been satisfied
+    @Optional
+    @Complexity(MEDIUM)
+    public static void dynamicDemand(Flux<String> source, CountDownLatch countDownOnComplete) {
+        // TODO: provide own implementation of Subscriber. Manage request count manually.
+        // TODO: two times increase request size each time when previous demand has been satisfied
 
-		// HINT: Consider usage of reactor.core.publisher.BaseSubscriber
-		// HINT: count down onComplete
+        // HINT: Consider usage of reactor.core.publisher.BaseSubscriber
+        // HINT: count down onComplete
 
-		throw new RuntimeException("Not implemented");
-	}
+
+
+    }
 }
