db81755ddd503887bd254e3605f3db7e617861be
dmitrypashkov <dmitry.pashkov@nextiva.com>
2018-11-25 16:27:06 +0200

    First Three Part One (my test)

--- work/chapter-1/src/main/java/com/example/part_3/Part3MultithreadingParallelization.java
+++ work/chapter-1/src/main/java/com/example/part_3/Part3MultithreadingParallelization.java
@@ -4,7 +4,9 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.ParallelFlux;
 import reactor.core.scheduler.Schedulers;
+import reactor.util.function.Tuple2;
 
+import java.time.Duration;
 import java.util.concurrent.Callable;
 
 import org.reactivestreams.Publisher;
@@ -34,6 +36,15 @@ public class Part3MultithreadingParallelization {
     @Complexity(HARD)
     public static Publisher<String> paralellizeLongRunningWorkOnUnboundedAmountOfThread(Flux<Callable<String>> streamOfLongRunningSources) {
         return streamOfLongRunningSources.parallel().runOn(Schedulers.elastic()).flatMap(Mono::fromCallable);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Flux<Integer> fluxWithDelay = Flux.range(1, 100).delaySubscription(Duration.ofMillis(1000));
+        Flux<Integer> fluxWithoutDelay = Flux.range(1, 100);
+        Flux<Tuple2<Integer, Integer>> tuple2Flux = fluxWithDelay.zipWith(fluxWithoutDelay);
+        tuple2Flux.subscribe(System.out::println);
+
+        Thread.sleep(10000);
     }
 
 }
