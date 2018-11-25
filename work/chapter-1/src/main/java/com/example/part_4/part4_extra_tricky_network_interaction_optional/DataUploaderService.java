4190a23c563dc456cdc8ec22d9e03873bdcb161f
dmitrypashkov <dmitry.pashkov@nextiva.com>
2018-11-25 23:43:37 +0200

    Home Work 4 / 1

--- work/chapter-1/src/main/java/com/example/part_4/part4_extra_tricky_network_interaction_optional/DataUploaderService.java
+++ work/chapter-1/src/main/java/com/example/part_4/part4_extra_tricky_network_interaction_optional/DataUploaderService.java
@@ -1,35 +1,41 @@
 package com.example.part_4.part4_extra_tricky_network_interaction_optional;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import java.time.Duration;
+import java.util.concurrent.TimeoutException;
 
 import com.example.annotations.Complexity;
 import com.example.annotations.Optional;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import static com.example.annotations.Complexity.Level.HARD;
 
 public class DataUploaderService {
 
-	private final HttpClient client;
+    private final HttpClient client;
 
-	public DataUploaderService(HttpClient client) {
-		this.client = client;
-	}
+    public DataUploaderService(HttpClient client) {
+        this.client = client;
+    }
 
-	@Optional
-	@Complexity(HARD)
-	public Mono<Void> upload(Flux<OrderedByteBuffer> input) {
-		// TODO: send data to a server using the given client
-		// TODO: MAX amount of sent buffers MUST be less or equals to 50 per request
-		// TODO: frequency of client#send invocation MUST be not often than once per 500 Milliseconds
-		// TODO: delivered results MUST be ordered
-		// TODO: in case if send operation take more than 1 second it MUST be considered as hanged and be restarted
+    //TODO
+    @Optional
+    @Complexity(HARD)
+    public Mono<Void> upload(Flux<OrderedByteBuffer> input) {
+        return input.windowTimeout(50, Duration.ofMillis(500))
+                .timeout(Duration.ofMillis(1000))
+                .retryBackoff(5, Duration.ofSeconds(5))
+                .concatMap(client::send)
+                .singleOrEmpty();
+        // TODO: send data to a server using the given client
+        // TODO: MAX amount of sent buffers MUST be less or equals to 50 per request
+        // TODO: frequency of client#send invocation MUST be not often than once per 500 Milliseconds
+        // TODO: delivered results MUST be ordered
+        // TODO: in case if send operation take more than 1 second it MUST be considered as hanged and be restarted
 
-		// HINT: consider usage of .windowTimeout, onBackpressureBuffer, concatMap, timeout, retryWhen or retryBackoff
+        // HINT: consider usage of .windowTimeout, onBackpressureBuffer, concatMap, timeout, retryWhen or retryBackoff
 
 
-
-		throw new RuntimeException("Not implemented");
-	}
+    }
 }
