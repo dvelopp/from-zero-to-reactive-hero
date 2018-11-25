1715093c743b7a3f211df242bbb448018bacc579
dmitrypashkov <dmitry.pashkov@nextiva.com>
2018-11-25 18:16:25 +0200

    Ex 5

--- work/chapter-1/src/main/java/com/example/part_5/Part5ResilienceResponsive.java
+++ work/chapter-1/src/main/java/com/example/part_5/Part5ResilienceResponsive.java
@@ -1,12 +1,16 @@
 package com.example.part_5;
 
-import com.example.annotations.Complexity;
-import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
+import java.time.Duration;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
+
+import org.reactivestreams.Publisher;
+
+import com.example.annotations.Complexity;
 
 import static com.example.annotations.Complexity.Level.EASY;
 import static com.example.annotations.Complexity.Level.HARD;
@@ -16,46 +20,26 @@ public class Part5ResilienceResponsive {
 
     @Complexity(EASY)
     public static Publisher<String> fallbackHelloOnEmpty(Flux<String> emptyPublisher) {
-        // TODO: return fallback on empty source
-        // TODO: in case of no value emitted return fallback with "Hello"
-        // HINT: Flux#switchIfEmpty() or Flux#defaultIfEmpty
-
-        throw new RuntimeException("Not implemented");
+        return emptyPublisher.defaultIfEmpty("Hello");
     }
 
     @Complexity(EASY)
     public static Publisher<String> fallbackHelloOnError(Flux<String> failurePublisher) {
-        // TODO: return fallback on error
-        // TODO: in case of error return fallback with "Hello"
-        // HINT: Flux#onErrorResume or Flux#onErrorReturn
-
-        throw new RuntimeException("Not implemented");
+        return failurePublisher.onErrorReturn("Hello");
     }
 
     @Complexity(EASY)
     public static Publisher<String> retryOnError(Mono<String> failurePublisher) {
-        // TODO: retry operation if error
-        // HINT: Flux#retry()
-
-        throw new RuntimeException("Not implemented");
+        return failurePublisher.retry();
     }
 
     @Complexity(MEDIUM)
     public static Publisher<String> timeoutLongOperation(CompletableFuture<String> longRunningCall) {
-        // TODO: limit the overall operation execution to one second
-        // TODO: in case of timeout return fallback with "Hello"
-        // HINT: Mono.fromFuture() + Mono#timeout(Duration, Mono)
-
-        throw new RuntimeException("Not implemented");
+        return Mono.fromFuture(longRunningCall).timeout(Duration.ofSeconds(1), Mono.just("Hello"));
     }
 
     @Complexity(HARD)
     public static Publisher<String> timeoutLongOperation(Callable<String> longRunningCall) {
-        // TODO: limit the overall operation execution to one second
-        // TODO: in case of timeout return fallback with "Hello"
-        // HINT: bear in mind that execution should occur on different thread
-        // HINT: Mono.fromCallable + .subscribeOn + Mono#timeout(Duration, Mono)
-
-        throw new RuntimeException("Not implemented");
+        return Mono.fromCallable(longRunningCall).subscribeOn(Schedulers.single()).timeout(Duration.ofSeconds(1), Mono.just("Hello"));
     }
 }
