ab7e7f0e8f3c2f93986ad8a871d1355c208e5fc4
dmitrypashkov <dmitry.pashkov@nextiva.com>
2018-11-25 21:58:48 +0200

    Home Work 2 / 1

--- work/chapter-1/src/main/java/com/example/part_2/part2_extra_distributed_media_library_optional/MediaService.java
+++ work/chapter-1/src/main/java/com/example/part_2/part2_extra_distributed_media_library_optional/MediaService.java
@@ -1,25 +1,24 @@
 package com.example.part_2.part2_extra_distributed_media_library_optional;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.example.annotations.Complexity;
 import com.example.annotations.Optional;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import static com.example.annotations.Complexity.Level.MEDIUM;
 
 public class MediaService {
 
-	private final List<Server> availableContentServers = Servers.list();
+    private final List<Server> availableContentServers = Servers.list();
 
-	@Optional
-	@Complexity(MEDIUM)
-	public Mono<Video> findVideo(String videoName) {
-		// TODO: using provided content servers search for video in every server and
-		//       return first found result
-		// HINT: consider Mono.first
-
-		throw new RuntimeException("Not implemented");
-	}
+    @Optional
+    @Complexity(MEDIUM)
+    public Mono<Video> findVideo(String videoName) {
+        return Mono.first(Flux.fromIterable(availableContentServers)
+                .map(server -> server.searchOne(videoName)).toStream().collect(Collectors.toList()));
+    }
 }
