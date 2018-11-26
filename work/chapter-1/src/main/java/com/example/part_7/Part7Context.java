e364dab17c564f5fa08449db8bc0891c2f670827
dmitrypashkov <dmitry.pashkov@nextiva.com>
2018-11-26 11:49:27 +0200

    7 / 1

--- work/chapter-1/src/main/java/com/example/part_7/Part7Context.java
+++ work/chapter-1/src/main/java/com/example/part_7/Part7Context.java
@@ -1,36 +1,35 @@
 package com.example.part_7;
 
-import com.example.annotations.Complexity;
-import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.context.Context;
+
+import org.reactivestreams.Publisher;
+
+import com.example.annotations.Complexity;
 
 import static com.example.annotations.Complexity.Level.EASY;
 
 public class Part7Context {
 
-	@Complexity(EASY)
-	public static Mono<String> grabDataFromTheGivenContext(Object key) {
-		// TODO: get data from the context
+    @Complexity(EASY)
+    public static Mono<String> grabDataFromTheGivenContext(Object key) {
+        return Mono.subscriberContext()
+                .filter(context -> context.hasKey(key))
+                .flatMap(context -> Mono.just(context.get(key)));
+    }
 
-		throw new RuntimeException("Not implemented");
-	}
+    @Complexity(EASY)
+    public static Mono<String> provideCorrectContext(Mono<String> source, Object key, Object value) {
+        return source.subscriberContext(context -> context.put(key, value));
+    }
 
-	@Complexity(EASY)
-	public static Mono<String> provideCorrectContext(Mono<String> source, Object key, Object value) {
-		// TODO: provide context for upstream source
-
-		throw new RuntimeException("Not implemented");
-	}
-
-	@Complexity(EASY)
-	public static Flux<String> provideCorrectContext(
-			Publisher<String> sourceA, Context contextA,
-			Publisher<String> sourceB, Context contextB) {
-		// TODO: edit without significant changes to provide corresponding context for each source
-
-		return Flux.from(sourceA)
-		           .mergeWith(sourceB);
-	}
+    @Complexity(EASY)
+    public static Flux<String> provideCorrectContext(
+            Publisher<String> sourceA, Context contextA,
+            Publisher<String> sourceB, Context contextB) {
+        // TODO: edit without significant changes to provide corresponding context for each source
+        return Flux.from(sourceA)
+                .mergeWith(sourceB);
+    }
 }
