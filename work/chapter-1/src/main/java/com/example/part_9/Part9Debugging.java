5d78d492f848ee2c50d6469a84bc04c7c43f59a2
dmitrypashkov <dmitry.pashkov@nextiva.com>
2018-11-26 12:16:08 +0200

    9 tests

--- work/chapter-1/src/main/java/com/example/part_9/Part9Debugging.java
+++ work/chapter-1/src/main/java/com/example/part_9/Part9Debugging.java
@@ -1,23 +1,27 @@
 package com.example.part_9;
 
-import com.example.annotations.Complexity;
-import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
 import reactor.test.StepVerifier;
+
+import org.junit.Test;
+
+import com.example.annotations.Complexity;
 
 import static com.example.annotations.Complexity.Level.MEDIUM;
 
 public class Part9Debugging {
 
-	@Test
-	@Complexity(MEDIUM)
-	public void checkMath() {
-		StepVerifier.create(Flux.range(0, 1234567)
-		                        .transform(ReactiveMath::doCalculation)
-								.take(1))
-		            .expectSubscription()
-		            .expectNextCount(1)
-		            .verifyComplete();
+    @Test
+    @Complexity(MEDIUM)
+    public void checkMath() {
+        Hooks.onOperatorDebug();
+        StepVerifier.create(Flux.range(0, 1234567)
+                .transform(ReactiveMath::doCalculation)
+                .take(1))
+                .expectSubscription()
+                .expectNextCount(1)
+                .verifyComplete();
 
-	}
+    }
 }
--- work/chapter-1/src/main/java/com/example/part_9/ReactiveMath.java
+++ work/chapter-1/src/main/java/com/example/part_9/ReactiveMath.java
@@ -4,9 +4,9 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class ReactiveMath {
-	public static Flux<Integer> doCalculation(Flux<Integer> range) {
-		return range
-				.scan(0, (aLong, aLong2) -> (aLong + aLong2 + 2 * aLong) / aLong2)
-				.flatMap( e -> Mono.just(1 / Mono.just(e).filter(p -> 121418 % p == 0).block()));
-	}
+    public static Flux<Integer> doCalculation(Flux<Integer> range) {
+        return range
+                .scan(1, (aLong, aLong2) -> (aLong + aLong2 + 2 * aLong) / aLong2)
+                .flatMap(e -> Mono.just(1 / Mono.just(e).filter(p -> 121418 % p == 0).checkpoint().block()));
+    }
 }
