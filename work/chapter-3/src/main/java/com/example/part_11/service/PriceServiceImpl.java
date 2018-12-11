0d4eeff9b12c94ad19fba0fc2947ed62e683c179
dmitrypashkov <dmitry.pashkov@nextiva.com>
2018-12-11 19:06:24 +0200

    Spring: Price Service Finish

--- work/chapter-3/src/main/java/com/example/part_11/service/PriceServiceImpl.java
+++ work/chapter-3/src/main/java/com/example/part_11/service/PriceServiceImpl.java
@@ -5,7 +5,9 @@
 package com.example.part_11.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.GroupedFlux;
 
+import java.time.Duration;
 import java.util.Map;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
@@ -14,11 +16,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.part_11.dto.MessageDTO;
+import com.example.part_11.utils.MessageMapper;
+import com.example.part_11.utils.Sum;
 
 @Service
 public class PriceServiceImpl implements PriceService {
 
     private static final Logger logger = Logger.getLogger("price-service");
+    private static final long DEFAULT_AVG_PRICE_INTERVAL = 30L;
 
     @Autowired
     private CryptoService cryptoService;
@@ -35,20 +40,44 @@ public class PriceServiceImpl implements PriceService {
     }
 
     private Flux<MessageDTO<Float>> currentPrice(Flux<Map<String, Object>> input) {
-        return null;
+        return input.map(MessageMapper::mapToPriceMessage);
     }
 
     private Flux<Map<String, Object>> selectOnlyPriceUpdateEvents(Flux<Map<String, Object>> input) {
-        return null;
+        return input
+                .filter(MessageMapper::isPriceMessageType)
+                .filter(MessageMapper::isValidPriceMessage);
     }
 
     @Override
     public Flux<MessageDTO<Float>> pricesStream(Flux<Long> intervalPreferencesStream) {
-        return null;
+        return sharedStream.transform(mainFlow -> Flux.merge(
+                mainFlow,
+                averagePrice(intervalPreferencesStream, mainFlow)
+        ));
     }
 
     public Flux<MessageDTO<Float>> getSharedStream() {
         return sharedStream;
     }
-    
+
+    Flux<MessageDTO<Float>> averagePrice(
+            Flux<Long> requestedInterval,
+            Flux<MessageDTO<Float>> priceData) {
+        return requestedInterval.startWith(DEFAULT_AVG_PRICE_INTERVAL)
+                .switchMap(timeInterval -> priceData.window(Duration.ofSeconds(timeInterval)))
+                .flatMap(flux -> {
+                            Flux<GroupedFlux<String, MessageDTO<Float>>> groupedFluxFlux = flux
+                                    .groupBy(MessageDTO::getCurrency);
+                            return groupedFluxFlux
+                                    .flatMap(keyFlux -> keyFlux
+                                            .map(MessageDTO::getData)
+                                            .reduce(Sum.empty(), Sum::add)
+                                            .map(Sum::avg)
+                                            .map(avg -> MessageDTO.avg(avg, keyFlux.key(), "Local Market")));
+                        }
+                );
+
+    }
+
 }
